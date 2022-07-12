aee384594bedcc9f654cad2123c6141c3ac40f5c
Jacob <encody@users.noreply.github.com>
2022-07-12 10:31:24 -0500

    chore: move common code to make_derive

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -8,6 +8,22 @@ mod pause;
 mod rbac;
 mod rename;
 
+fn make_derive<T>(
+    input: TokenStream,
+    expand: fn(T) -> Result<TokenStream, syn::Error>,
+) -> TokenStream
+where
+    T: FromDeriveInput,
+{
+    let input = parse_macro_input!(input as DeriveInput);
+    let meta: T = match FromDeriveInput::from_derive_input(&input) {
+        Err(e) => return e.write_errors().into(),
+        Ok(x) => x,
+    };
+
+    expand(meta).unwrap_or_else(|e| e.into_compile_error().into())
+}
+
 /// Derives an NEP-297-compatible event emitting implementation of `Event`.
 ///
 /// Specify event standard parameters: `#[event(standard = "...", version = "...")]`
@@ -23,10 +39,7 @@ mod rename;
 /// - `Title Case`
 #[proc_macro_derive(Event, attributes(event))]
 pub fn derive_event(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let meta: event::EventMeta = FromDeriveInput::from_derive_input(&input).unwrap();
-
-    event::expand(meta).unwrap_or_else(|e| e.into_compile_error().into())
+    make_derive(input, event::expand)
 }
 
 /// Creates a managed, lazily-loaded `Owner` implementation for the targeted
@@ -34,16 +47,9 @@ pub fn derive_event(input: TokenStream) -> TokenStream {
 ///
 /// The storage key prefix for the fields can be optionally specified (default:
 /// `"~o"`) using `#[owner(storage_key = "<expression>")]`.
-/// ```
 #[proc_macro_derive(Owner, attributes(owner))]
 pub fn derive_owner(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let meta = match FromDeriveInput::from_derive_input(&input) {
-        Err(e) => return e.write_errors().into(),
-        Ok(x) => x,
-    };
-
-    owner::expand(meta).unwrap_or_else(|e| e.into_compile_error().into())
+    make_derive(input, owner::expand)
 }
 
 /// Makes a contract pausable. Provides an implementation of the `Pause` trait.
@@ -52,13 +58,7 @@ pub fn derive_owner(input: TokenStream) -> TokenStream {
 /// `"~p"`) using `#[pause(storage_key = "<expression>")]`.
 #[proc_macro_derive(Pause, attributes(pause))]
 pub fn derive_pause(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let meta = match FromDeriveInput::from_derive_input(&input) {
-        Err(e) => return e.write_errors().into(),
-        Ok(x) => x,
-    };
-
-    pause::expand(meta).unwrap_or_else(|e| e.into_compile_error().into())
+    make_derive(input, pause::expand)
 }
 
 /// Adds role-based access control. No external methods are exposed.
@@ -67,11 +67,5 @@ pub fn derive_pause(input: TokenStream) -> TokenStream {
 /// `"~r"`) using `#[rbac(storage_key = "<expression>")]`.
 #[proc_macro_derive(Rbac, attributes(rbac))]
 pub fn derive_rbac(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let meta = match FromDeriveInput::from_derive_input(&input) {
-        Err(e) => return e.write_errors().into(),
-        Ok(x) => x,
-    };
-
-    rbac::expand(meta).unwrap_or_else(|e| e.into_compile_error().into())
+    make_derive(input, rbac::expand)
 }
