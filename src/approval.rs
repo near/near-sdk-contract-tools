7037267054cedf066e04433045dd0157b84e3516
Jacob <encody@users.noreply.github.com>
2022-08-23 20:55:09 -0700

    chore: rename confirmation -> approval

--- src/approval.rs
+++ src/approval.rs
@@ -8,7 +8,7 @@ use near_sdk::{
 };
 use serde::{Deserialize, Serialize};
 
-/// Actions can be executed after they are confirmed
+/// Actions can be executed after they are approved
 pub trait Action {
     /// Return type of the action. Useful if the action creates a `Promise`, for example.
     type Output;
@@ -16,21 +16,21 @@ pub trait Action {
     fn execute(self) -> Self::Output;
 }
 
-/// The confirmation state determines whether an action request has achieved
-/// sufficient confirmations. For example, multisig confirmation state would
-/// keep track of who has approved an action request so far.
-pub trait ConfirmationState<C> {
-    /// Whether the current state represents full confirmation. Note that this
+/// The approval state determines whether an action request has achieved
+/// sufficient approvals. For example, multisig confirmation state would keep
+/// track of who has approved an action request so far.
+pub trait ApprovalState<C> {
+    /// Whether the current state represents full approval. Note that this
     /// function is called immediately before attempting to execute an action,
     /// so it is possible for this function to respond to externalities (i.e.
-    /// changes to contract state other than calls to confirm or reject)
-    fn is_confirmed(&self, config: &C) -> bool;
+    /// changes to contract state other than calls to approve or reject)
+    fn is_approved(&self, config: &C) -> bool;
 
-    /// Try to improve the confirmation state. Additional arguments may be
+    /// Try to improve the approval state. Additional arguments may be
     /// provided, e.g. from the initiating function call
-    fn attempt_confirmation(&mut self, args: Option<String>, config: &C);
+    fn attempt_approval(&mut self, args: Option<String>, config: &C);
 
-    /// Try to worsen the confirmation state. Additional arguments may be
+    /// Try to worsen the approval state. Additional arguments may be
     /// provided, e.g. from the initiating function call
     fn attempt_rejection(&mut self, _args: Option<String>, _config: &C) -> bool {
         false
@@ -38,30 +38,30 @@ pub trait ConfirmationState<C> {
 }
 
 /// An action request is composed of an action that will be executed when the
-/// associated confirmation state is satisfied
+/// associated approval state is satisfied
 #[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Debug)]
 pub struct ActionRequest<A, S> {
-    /// The action that will be executed when the confirmation state is
+    /// The action that will be executed when the approval state is
     /// fulfilled
     pub action: A,
-    /// The associated confirmation state
-    pub confirmation_state: S,
+    /// The associated approval state
+    pub approval_state: S,
 }
 
-/// Collection of action requests that manages their confirmation state and
+/// Collection of action requests that manages their approval state and
 /// execution
 #[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Default)]
-pub struct Confirmations<A, S, C> {
+pub struct Approval<A, S, C> {
     /// Because requests will be deleted from the requests collection,
     /// maintain a simple counter to guarantee unique IDs
     pub next_request_id: u32,
     /// Current list of pending action requests.
     pub requests: HashMap<u32, ActionRequest<A, S>>,
-    /// Confirmation context included in relevant confirmation-related calls
+    /// Approval context included in relevant approval-related calls
     pub config: C,
 }
 
-impl<A, S, C> Confirmations<A, S, C> {
+impl<A, S, C> Approval<A, S, C> {
     /// Creates a new instance of the struct with the given config
     pub fn new(config: C) -> Self {
         Self {
@@ -71,9 +71,8 @@ impl<A, S, C> Confirmations<A, S, C> {
         }
     }
 
-    /// Creates a new action request initialized with the given confirmation
-    /// state
-    pub fn add_request_with_state(&mut self, action: A, confirmation_state: S) -> u32 {
+    /// Creates a new action request initialized with the given approval state
+    pub fn add_request_with_state(&mut self, action: A, approval_state: S) -> u32 {
         let request_id = self.next_request_id;
         self.next_request_id += 1;
 
@@ -81,7 +80,7 @@ impl<A, S, C> Confirmations<A, S, C> {
             request_id,
             ActionRequest {
                 action,
-                confirmation_state,
+                approval_state,
             },
         );
 
@@ -89,8 +88,8 @@ impl<A, S, C> Confirmations<A, S, C> {
     }
 }
 
-impl<A, S: Default, C> Confirmations<A, S, C> {
-    /// Creates a new action request with the default confirmation state
+impl<A, S: Default, C> Approval<A, S, C> {
+    /// Creates a new action request with the default approval state
     pub fn add_request(&mut self, action: A) -> u32 {
         let request_id = self.next_request_id;
         self.next_request_id += 1;
@@ -99,7 +98,7 @@ impl<A, S: Default, C> Confirmations<A, S, C> {
             request_id,
             ActionRequest {
                 action,
-                confirmation_state: Default::default(),
+                approval_state: Default::default(),
             },
         );
 
@@ -107,13 +106,13 @@ impl<A, S: Default, C> Confirmations<A, S, C> {
     }
 }
 
-impl<A: Action, S: ConfirmationState<C>, C> Confirmations<A, S, C> {
+impl<A: Action, S: ApprovalState<C>, C> Approval<A, S, C> {
     /// Executes an action request and removes it from the collection if the
-    /// confirmation state of the request is fulfilled. Panics otherwise.
+    /// approval state of the request is fulfilled. Panics otherwise.
     pub fn attempt_execution(&mut self, request_id: u32) -> A::Output {
         require!(
-            self.is_confirmed(request_id),
-            "Request must be confirmed before it can be executed",
+            self.is_approved(request_id),
+            "Request must be approved before it can be executed",
         );
 
         self.requests
@@ -123,24 +122,24 @@ impl<A: Action, S: ConfirmationState<C>, C> Confirmations<A, S, C> {
     }
 }
 
-impl<A, S: ConfirmationState<C>, C> Confirmations<A, S, C> {
-    /// Returns `true` if the given request ID exists and is confirmed (that
+impl<A, S: ApprovalState<C>, C> Approval<A, S, C> {
+    /// Returns `true` if the given request ID exists and is approved (that
     /// is, the action request may be executed), `false` otherwise.
-    pub fn is_confirmed(&self, request_id: u32) -> bool {
+    pub fn is_approved(&self, request_id: u32) -> bool {
         self.requests
             .get(&request_id)
-            .map(|request| request.confirmation_state.is_confirmed(&self.config))
+            .map(|request| request.approval_state.is_approved(&self.config))
             .unwrap_or(false)
     }
 
-    /// Tries to confirm the action request designated by the given request ID
+    /// Tries to approve the action request designated by the given request ID
     /// with the given arguments. Panics if the request ID does not exist.
-    pub fn attempt_confirmation(&mut self, request_id: u32, args: Option<String>) {
+    pub fn attempt_approval(&mut self, request_id: u32, args: Option<String>) {
         self.requests
             .get_mut(&request_id)
             .unwrap()
-            .confirmation_state
-            .attempt_confirmation(args, &self.config);
+            .approval_state
+            .attempt_approval(args, &self.config);
     }
 
     /// Tries to reject the action request designated by the given request ID
@@ -149,7 +148,7 @@ impl<A, S: ConfirmationState<C>, C> Confirmations<A, S, C> {
         self.requests
             .get_mut(&request_id)
             .unwrap()
-            .confirmation_state
+            .approval_state
             .attempt_rejection(args, &self.config)
             .then(|| {
                 self.requests.remove(&request_id);
@@ -168,9 +167,9 @@ mod tests {
     };
 
     use crate::near_contract_tools;
-    use crate::{confirmations::ConfirmationState, rbac::Rbac};
+    use crate::{approval::ApprovalState, rbac::Rbac};
 
-    use super::{Action, Confirmations};
+    use super::{Action, Approval};
 
     #[derive(BorshSerialize, BorshStorageKey)]
     enum Role {
@@ -204,7 +203,7 @@ mod tests {
     #[rbac(roles = "Role")]
     #[near_bindgen]
     struct Contract {
-        pub confirmations: Confirmations<MyAction, MultisigConfirmationState, MultisigConfig>,
+        pub approval: Approval<MyAction, MultisigApprovalState, MultisigConfig>,
     }
 
     #[derive(Debug)]
@@ -213,13 +212,13 @@ mod tests {
     }
 
     #[derive(Default, Debug)]
-    struct MultisigConfirmationState {
-        pub confirmed_by: Vec<AccountId>,
+    struct MultisigApprovalState {
+        pub approved_by: Vec<AccountId>,
     }
 
-    impl ConfirmationState<MultisigConfig> for MultisigConfirmationState {
-        fn is_confirmed(&self, config: &MultisigConfig) -> bool {
-            self.confirmed_by
+    impl ApprovalState<MultisigConfig> for MultisigApprovalState {
+        fn is_approved(&self, config: &MultisigConfig) -> bool {
+            self.approved_by
                 .iter()
                 .filter(|account| {
                     // in case a signatory's role was revoked in the meantime
@@ -229,18 +228,18 @@ mod tests {
                 >= config.threshold as usize
         }
 
-        fn attempt_confirmation(&mut self, _args: Option<String>, _config: &MultisigConfig) {
+        fn attempt_approval(&mut self, _args: Option<String>, _config: &MultisigConfig) {
             let predecessor = env::predecessor_account_id();
             require!(
                 Contract::has_role(&predecessor, &Role::Multisig),
                 "Must have multisig role",
             );
             require!(
-                !self.confirmed_by.contains(&predecessor),
-                "Already confirmed by this account",
+                !self.approved_by.contains(&predecessor),
+                "Already approved by this account",
             );
 
-            self.confirmed_by.push(predecessor);
+            self.approved_by.push(predecessor);
         }
 
         fn attempt_rejection(&mut self, _args: Option<String>, _config: &MultisigConfig) -> bool {
@@ -250,10 +249,10 @@ mod tests {
                 "Must have multisig role",
             );
 
-            self.confirmed_by
+            self.approved_by
                 .retain(|signatory| signatory != &predecessor);
 
-            self.confirmed_by.len() == 0
+            self.approved_by.len() == 0
         }
     }
 
@@ -264,125 +263,106 @@ mod tests {
     }
 
     #[test]
-    fn successful_confirmation() {
+    fn successful_approval() {
         let alice: AccountId = "alice".parse().unwrap();
         let bob: AccountId = "bob_acct".parse().unwrap();
         let charlie: AccountId = "charlie".parse().unwrap();
 
         let mut contract = Contract {
-            confirmations: Confirmations::new(MultisigConfig { threshold: 2 }),
+            approval: Approval::new(MultisigConfig { threshold: 2 }),
         };
 
         contract.add_role(&alice, &Role::Multisig);
         contract.add_role(&bob, &Role::Multisig);
         contract.add_role(&charlie, &Role::Multisig);
 
-        let request_id = contract.confirmations.add_request(MyAction::SayHello);
+        let request_id = contract.approval.add_request(MyAction::SayHello);
 
         assert_eq!(request_id, 0);
-        assert!(!contract.confirmations.is_confirmed(request_id));
+        assert!(!contract.approval.is_approved(request_id));
 
         predecessor(&alice);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        assert!(!contract.confirmations.is_confirmed(request_id));
+        assert!(!contract.approval.is_approved(request_id));
 
         predecessor(&charlie);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        assert!(contract.confirmations.is_confirmed(request_id));
+        assert!(contract.approval.is_approved(request_id));
 
-        assert_eq!(
-            contract.confirmations.attempt_execution(request_id),
-            "hello",
-        );
+        assert_eq!(contract.approval.attempt_execution(request_id), "hello",);
     }
 
     #[test]
-    #[should_panic(expected = "Already confirmed by this account")]
-    fn duplicate_confirmation() {
+    #[should_panic(expected = "Already approved by this account")]
+    fn duplicate_approval() {
         let alice: AccountId = "alice".parse().unwrap();
 
         let mut contract = Contract {
-            confirmations: Confirmations::new(MultisigConfig { threshold: 2 }),
+            approval: Approval::new(MultisigConfig { threshold: 2 }),
         };
 
         contract.add_role(&alice, &Role::Multisig);
 
-        let request_id = contract.confirmations.add_request(MyAction::SayHello);
+        let request_id = contract.approval.add_request(MyAction::SayHello);
 
         predecessor(&alice);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
     }
 
     #[test]
-    #[should_panic = "Request must be confirmed before it can be executed"]
-    fn no_execution_before_confirmation() {
+    #[should_panic = "Request must be approved before it can be executed"]
+    fn no_execution_before_approval() {
         let alice: AccountId = "alice".parse().unwrap();
 
         let mut contract = Contract {
-            confirmations: Confirmations::new(MultisigConfig { threshold: 2 }),
+            approval: Approval::new(MultisigConfig { threshold: 2 }),
         };
 
         contract.add_role(&alice, &Role::Multisig);
 
-        let request_id = contract.confirmations.add_request(MyAction::SayHello);
+        let request_id = contract.approval.add_request(MyAction::SayHello);
 
         predecessor(&alice);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        contract.confirmations.attempt_execution(request_id);
+        contract.approval.attempt_execution(request_id);
     }
 
     #[test]
-    fn dynamic_confirmation_calculation() {
+    fn dynamic_is_approved_calculation() {
         let alice: AccountId = "alice".parse().unwrap();
         let bob: AccountId = "bob_acct".parse().unwrap();
         let charlie: AccountId = "charlie".parse().unwrap();
 
         let mut contract = Contract {
-            confirmations: Confirmations::new(MultisigConfig { threshold: 2 }),
+            approval: Approval::new(MultisigConfig { threshold: 2 }),
         };
 
         contract.add_role(&alice, &Role::Multisig);
         contract.add_role(&bob, &Role::Multisig);
         contract.add_role(&charlie, &Role::Multisig);
 
-        let request_id = contract.confirmations.add_request(MyAction::SayGoodbye);
+        let request_id = contract.approval.add_request(MyAction::SayGoodbye);
 
         predecessor(&alice);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
         predecessor(&bob);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        assert!(contract.confirmations.is_confirmed(request_id));
+        assert!(contract.approval.is_approved(request_id));
 
         contract.remove_role(&alice, &Role::Multisig);
 
-        assert!(!contract.confirmations.is_confirmed(request_id));
+        assert!(!contract.approval.is_approved(request_id));
 
         predecessor(&charlie);
-        contract
-            .confirmations
-            .attempt_confirmation(request_id, None);
+        contract.approval.attempt_approval(request_id, None);
 
-        assert!(contract.confirmations.is_confirmed(request_id));
+        assert!(contract.approval.is_approved(request_id));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 
 pub mod standard;
 
-pub mod confirmations;
+pub mod approval;
 pub mod event;
 pub mod migrate;
 pub mod owner;
