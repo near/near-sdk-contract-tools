f4116bbd7d1c249adf1070b5d6605219ac187db7
Jacob <encody@users.noreply.github.com>
2022-08-25 19:14:20 +0900

    chore: approval uses native NEAR storage APIs

--- src/approval/mod.rs
+++ src/approval/mod.rs
@@ -1,18 +1,20 @@
 //! Queue and approve actions
 
-use std::collections::HashMap;
+use std::fmt::Debug;
 
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
-    require,
+    require, BorshStorageKey,
 };
 use serde::{Deserialize, Serialize};
 
-pub mod expire_multisig;
+use crate::slot::Slot;
+
+// pub mod expire_multisig;
 pub mod simple_multisig;
 
 /// Actions can be executed after they are approved
-pub trait Action {
+pub trait Action: Debug + BorshSerialize + BorshDeserialize {
     /// Return type of the action. Useful if the action creates a `Promise`, for example.
     type Output;
     /// Perform the action. One time only.
@@ -22,7 +24,7 @@ pub trait Action {
 /// The approval state determines whether an action request has achieved
 /// sufficient approvals. For example, multisig confirmation state would keep
 /// track of who has approved an action request so far.
-pub trait ApprovalState<C> {
+pub trait ApprovalState<C>: Debug + Default + BorshSerialize + BorshDeserialize {
     /// Whether the current state represents full approval. Note that this
     /// function is called immediately before attempting to execute an action,
     /// so it is possible for this function to respond to externalities (i.e.
@@ -51,111 +53,129 @@ pub struct ActionRequest<A, S> {
     pub approval_state: S,
 }
 
-/// Collection of action requests that manages their approval state and
-/// execution
-#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Default)]
-pub struct Approval<A, S, C> {
-    /// Because requests will be deleted from the requests collection,
-    /// maintain a simple counter to guarantee unique IDs
-    pub next_request_id: u32,
-    /// Current list of pending action requests.
-    pub requests: HashMap<u32, ActionRequest<A, S>>,
-    /// Approval context included in relevant approval-related calls
-    pub config: C,
+#[derive(BorshSerialize, BorshStorageKey)]
+enum ApprovalStorageKey {
+    NextRequestId,
+    Config,
+    Request(u32),
 }
 
-impl<A, S, C> Approval<A, S, C> {
-    /// Creates a new instance of the struct with the given config
-    pub fn new(config: C) -> Self {
-        Self {
-            next_request_id: 0,
-            requests: Default::default(),
-            config,
-        }
+/// Collection of action requests that manages their approval state and
+/// execution
+pub trait Approval<A, S, C>
+where
+    A: Action,
+    S: ApprovalState<C>,
+    C: BorshDeserialize + BorshSerialize,
+{
+    /// Storage root
+    fn root() -> Slot<()>;
+
+    /// Because requests will be deleted from the requests collection,
+    /// maintain a simple counter to guarantee unique IDs
+    fn slot_next_request_id() -> Slot<u32> {
+        Self::root().field(ApprovalStorageKey::NextRequestId)
+    }
+
+    /// Approval context included in relevant approval-related calls
+    fn slot_config() -> Slot<C> {
+        Self::root().field(ApprovalStorageKey::Config)
+    }
+
+    /// Current list of pending action requests.
+    fn slot_request(request_id: u32) -> Slot<ActionRequest<A, S>> {
+        Self::root().field(ApprovalStorageKey::Request(request_id))
+    }
+
+    /// Must be called before using the Approval construct. Can only be called
+    /// once.
+    fn init(config: C) {
+        require!(
+            Self::slot_config().swap(&config).is_none(),
+            "Already initialized",
+        );
     }
 
     /// Creates a new action request initialized with the given approval state
-    pub fn add_request_with_state(&mut self, action: A, approval_state: S) -> u32 {
-        let request_id = self.next_request_id;
-        self.next_request_id += 1;
+    fn add_request_with_state(&mut self, action: A, approval_state: S) -> u32 {
+        let request_id = Self::slot_next_request_id().read().unwrap_or(0);
+        Self::slot_next_request_id().write(&(request_id + 1));
 
-        self.requests.insert(
-            request_id,
-            ActionRequest {
-                action,
-                approval_state,
-            },
-        );
+        Self::slot_request(request_id).write(&ActionRequest {
+            action,
+            approval_state,
+        });
 
         request_id
     }
-}
 
-impl<A, S: Default, C> Approval<A, S, C> {
     /// Creates a new action request with the default approval state
-    pub fn add_request(&mut self, action: A) -> u32 {
-        let request_id = self.next_request_id;
-        self.next_request_id += 1;
+    fn add_request(&mut self, action: A) -> u32 {
+        let request_id = Self::slot_next_request_id().read().unwrap_or(0);
+        Self::slot_next_request_id().write(&(request_id + 1));
 
-        self.requests.insert(
-            request_id,
-            ActionRequest {
-                action,
-                approval_state: Default::default(),
-            },
-        );
+        Self::slot_request(request_id).write(&ActionRequest {
+            action,
+            approval_state: Default::default(),
+        });
 
         request_id
     }
-}
 
-impl<A: Action, S: ApprovalState<C>, C> Approval<A, S, C> {
     /// Executes an action request and removes it from the collection if the
     /// approval state of the request is fulfilled. Panics otherwise.
-    pub fn attempt_execution(&mut self, request_id: u32) -> A::Output {
+    fn attempt_execution(&mut self, request_id: u32) -> A::Output {
         require!(
-            self.is_approved(request_id),
+            Self::is_approved(request_id),
             "Request must be approved before it can be executed",
         );
 
-        self.requests
-            .remove(&request_id)
+        Self::slot_request(request_id)
+            .take()
             .map(|request| request.action.execute())
             .unwrap()
     }
-}
 
-impl<A, S: ApprovalState<C>, C> Approval<A, S, C> {
     /// Returns `true` if the given request ID exists and is approved (that
     /// is, the action request may be executed), `false` otherwise.
-    pub fn is_approved(&self, request_id: u32) -> bool {
-        self.requests
-            .get(&request_id)
-            .map(|request| request.approval_state.is_approved(&self.config))
+    fn is_approved(request_id: u32) -> bool {
+        Self::slot_request(request_id)
+            .read()
+            .map(|request| {
+                let config = Self::slot_config().read().unwrap();
+                request.approval_state.is_approved(&config)
+            })
             .unwrap_or(false)
     }
 
     /// Tries to approve the action request designated by the given request ID
     /// with the given arguments. Panics if the request ID does not exist.
-    pub fn attempt_approval(&mut self, request_id: u32, args: Option<String>) {
-        self.requests
-            .get_mut(&request_id)
-            .unwrap()
+    fn attempt_approval(&mut self, request_id: u32, args: Option<String>) {
+        let mut request_slot = Self::slot_request(request_id);
+        let mut request = request_slot.read().unwrap();
+
+        request
             .approval_state
-            .attempt_approval(args, &self.config);
+            .attempt_approval(args, &Self::slot_config().read().unwrap());
+
+        request_slot.write(&request);
     }
 
     /// Tries to reject the action request designated by the given request ID
     /// with the given arguments. Panics if the request ID does not exist.
-    pub fn attempt_rejection(&mut self, request_id: u32, args: Option<String>) {
-        self.requests
-            .get_mut(&request_id)
-            .unwrap()
+    fn attempt_rejection(&mut self, request_id: u32, args: Option<String>) {
+        let mut request_slot = Self::slot_request(request_id);
+        let mut request = request_slot.read().unwrap();
+
+        let completely_rejected = request
             .approval_state
-            .attempt_rejection(args, &self.config)
-            .then(|| {
-                self.requests.remove(&request_id);
-            });
+            .attempt_rejection(args, &Self::slot_config().read().unwrap());
+
+        if completely_rejected {
+            request_slot.remove();
+        } else {
+            request_slot.write(&request);
+        }
     }
 }
 
@@ -163,14 +183,14 @@ impl<A, S: ApprovalState<C>, C> Approval<A, S, C> {
 mod tests {
     use near_contract_tools_macros::Rbac;
     use near_sdk::{
-        borsh::{self, BorshSerialize},
+        borsh::{self, BorshDeserialize, BorshSerialize},
         env, near_bindgen, require,
         test_utils::VMContextBuilder,
         testing_env, AccountId, BorshStorageKey,
     };
 
-    use crate::near_contract_tools;
     use crate::{approval::ApprovalState, rbac::Rbac};
+    use crate::{near_contract_tools, slot::Slot};
 
     use super::{Action, Approval};
 
@@ -179,7 +199,7 @@ mod tests {
         Multisig,
     }
 
-    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    #[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq, Clone, Copy)]
     enum MyAction {
         SayHello,
         SayGoodbye,
@@ -205,16 +225,32 @@ mod tests {
     #[derive(Rbac)]
     #[rbac(roles = "Role")]
     #[near_bindgen]
-    struct Contract {
-        pub approval: Approval<MyAction, MultisigApprovalState, MultisigConfig>,
+    struct Contract {}
+
+    #[near_bindgen]
+    impl Contract {
+        #[init]
+        pub fn new(threshold: u8) -> Self {
+            let contract = Self {};
+
+            <Self as Approval<_, _, _>>::init(MultisigConfig { threshold });
+
+            contract
+        }
     }
 
-    #[derive(Debug)]
+    impl Approval<MyAction, MultisigApprovalState, MultisigConfig> for Contract {
+        fn root() -> Slot<()> {
+            Slot::new(b"a")
+        }
+    }
+
+    #[derive(BorshSerialize, BorshDeserialize, Debug)]
     struct MultisigConfig {
         pub threshold: u8,
     }
 
-    #[derive(Default, Debug)]
+    #[derive(BorshSerialize, BorshDeserialize, Default, Debug)]
     struct MultisigApprovalState {
         pub approved_by: Vec<AccountId>,
     }
@@ -271,30 +307,28 @@ mod tests {
         let bob: AccountId = "bob_acct".parse().unwrap();
         let charlie: AccountId = "charlie".parse().unwrap();
 
-        let mut contract = Contract {
-            approval: Approval::new(MultisigConfig { threshold: 2 }),
-        };
+        let mut contract = Contract::new(2);
 
         contract.add_role(&alice, &Role::Multisig);
         contract.add_role(&bob, &Role::Multisig);
         contract.add_role(&charlie, &Role::Multisig);
 
-        let request_id = contract.approval.add_request(MyAction::SayHello);
+        let request_id = contract.add_request(MyAction::SayHello);
 
         assert_eq!(request_id, 0);
-        assert!(!contract.approval.is_approved(request_id));
+        assert!(!Contract::is_approved(request_id));
 
         predecessor(&alice);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        assert!(!contract.approval.is_approved(request_id));
+        assert!(!Contract::is_approved(request_id));
 
         predecessor(&charlie);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        assert!(contract.approval.is_approved(request_id));
+        assert!(Contract::is_approved(request_id));
 
-        assert_eq!(contract.approval.attempt_execution(request_id), "hello",);
+        assert_eq!(contract.attempt_execution(request_id), "hello",);
     }
 
     #[test]
@@ -302,18 +336,16 @@ mod tests {
     fn duplicate_approval() {
         let alice: AccountId = "alice".parse().unwrap();
 
-        let mut contract = Contract {
-            approval: Approval::new(MultisigConfig { threshold: 2 }),
-        };
+        let mut contract = Contract::new(2);
 
         contract.add_role(&alice, &Role::Multisig);
 
-        let request_id = contract.approval.add_request(MyAction::SayHello);
+        let request_id = contract.add_request(MyAction::SayHello);
 
         predecessor(&alice);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
     }
 
     #[test]
@@ -321,18 +353,16 @@ mod tests {
     fn no_execution_before_approval() {
         let alice: AccountId = "alice".parse().unwrap();
 
-        let mut contract = Contract {
-            approval: Approval::new(MultisigConfig { threshold: 2 }),
-        };
+        let mut contract = Contract::new(2);
 
         contract.add_role(&alice, &Role::Multisig);
 
-        let request_id = contract.approval.add_request(MyAction::SayHello);
+        let request_id = contract.add_request(MyAction::SayHello);
 
         predecessor(&alice);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        contract.approval.attempt_execution(request_id);
+        contract.attempt_execution(request_id);
     }
 
     #[test]
@@ -341,31 +371,29 @@ mod tests {
         let bob: AccountId = "bob_acct".parse().unwrap();
         let charlie: AccountId = "charlie".parse().unwrap();
 
-        let mut contract = Contract {
-            approval: Approval::new(MultisigConfig { threshold: 2 }),
-        };
+        let mut contract = Contract::new(2);
 
         contract.add_role(&alice, &Role::Multisig);
         contract.add_role(&bob, &Role::Multisig);
         contract.add_role(&charlie, &Role::Multisig);
 
-        let request_id = contract.approval.add_request(MyAction::SayGoodbye);
+        let request_id = contract.add_request(MyAction::SayGoodbye);
 
         predecessor(&alice);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
         predecessor(&bob);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        assert!(contract.approval.is_approved(request_id));
+        assert!(Contract::is_approved(request_id));
 
         contract.remove_role(&alice, &Role::Multisig);
 
-        assert!(!contract.approval.is_approved(request_id));
+        assert!(!Contract::is_approved(request_id));
 
         predecessor(&charlie);
-        contract.approval.attempt_approval(request_id, None);
+        contract.attempt_approval(request_id, None);
 
-        assert!(contract.approval.is_approved(request_id));
+        assert!(Contract::is_approved(request_id));
     }
 }
